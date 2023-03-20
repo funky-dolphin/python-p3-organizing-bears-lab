809a30d26635db68f6955c08736ee171e50802e1
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-03-20 16:26:36 -0400

    Lab Complete

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id integer Primary Key,
+    name text, 
+    age integer not null, 
+    sex text not null, 
+    color text not null, 
+    temperament text not null, 
+    alive boolean not null 
+);
---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+Insert into bears( name, age, sex, color, temperament, alive) 
+values
+("Mr. Chocolate", 2, "M", "brown", "chill", true),
+("Rowdy", 4, "F", "Black", "hostile", true),
+("Tabitha", 4, "F", "Black", "hostile", true),
+("Sergeant Brown", 4, "F", "Black", "hostile", true),
+("Melissa", 4, "F", "Black", "hostile", true),
+("Grinch", 4, "F", "Black", "hostile", true),
+("Wendy", 4, "F", "Black", "hostile", true),
+(null, 4, "F", "Black", "hostile", true);
+
